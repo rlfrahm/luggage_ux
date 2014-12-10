71b2d9047954a586401c4bb7ec5fa519afc39f33
rlfrahm <frahmryan@gmail.com>
2014-12-10 10:42:56 -0600

    Added css for hover effects and fixed bug in js

--- js/block_collapse.js
+++ js/block_collapse.js
@@ -9,10 +9,10 @@
     Drupal.behaviors.luggageUxBlockCollapse = {
         attach: function (context) {
             $('body', context).once('luggageUxBlockCollapse', function() {
-                $('aside section.block').addClass('js-suitcase-facet');
+                $('aside section.block').addClass('luggage_ux-block-facet');
                 var touched = false;
 
-                $('.js-suitcase-facet .block-title').each(function() {
+                $('.luggage_ux-block-facet .block-title').each(function() {
                     $(this).append('<span class="caret"></span>');
                 }).bind('click touchend', function(e) {
                     if(touched) {e.preventDefault();touched=false;return;}
@@ -21,7 +21,7 @@
                 });
 
                 if($(window).width() < 980) {
-                    $('.js-suitcase-facet .content').toggle();
+                    $('.luggage_ux-block-facet .content').toggle();
                 }
             });
         }
